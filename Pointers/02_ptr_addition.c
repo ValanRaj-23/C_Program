f4f49f90fd7e6a46a5a8460affd43edcb4675f66
VALAN RAJ S <valanrajs.2364@gmail.com>
2025-09-03 00:03:03 +0530

    Update 02_ptr_addition.c

--- Pointers/02_ptr_addition.c
+++ Pointers/02_ptr_addition.c
@@ -4,12 +4,19 @@ int main()
 	int arr[] = {0, 1, 2, 3, 4, 5};
 	int *ptr  = &arr[0];
 	
-	printf("Value in ptr address %d", *ptr);
-	printf("addr of array element", ptr);
+	printf("Value in ptr address %d\n", *ptr);
+	printf("addr of array element %p\n", ptr);
 
 	
-	printf("Value in ptr address %d", *(ptr+2));
-	printf("addr of array element", ptr+2);
+	printf("Value in ptr address %d\n", *(ptr+2));
+	printf("addr of array element %p\n", ptr+2);
 
 }
 
+
+/*
+Value in ptr address 0
+addr of array element 0x7ffcd168a800
+Value in ptr address 2
+addr of array element 0x7ffcd168a808
+*/
